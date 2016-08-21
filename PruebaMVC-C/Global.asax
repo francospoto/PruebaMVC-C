98a9273b4f881bdfc84555d9a1ca4bea8dfefc30
Gerardo TML <Gerardo TML@DESKTOP-0F84MC4>
2016-08-20 23:22:31 -0300

    Agregar archivos de proyecto.

---
+++ PruebaMVC-C/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PruebaMVC_C.MvcApplication" Language="C#" %>
---
+++ PruebaMVC-C/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PruebaMVC_C.Startup))]
+namespace PruebaMVC_C
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
